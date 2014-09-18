ba6d4942573163f9babe3a99d5ac497e367c13eb
Fabien Amarger <fabien.amarger@gmail.com>
2014-09-18 12:52:15 +0200

    First push

---
+++ src/Candidate/Candidate.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Candidate;
+
+import Source.Source;
+import com.mongodb.BasicDBObject;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Fabien
+ */
+public abstract class Candidate 
+{
+    protected float trustScore = 0;
+    protected HashMap<Source,  String> uriImplicate;
+    
+    public abstract void addElem(Source s, String uriElem);
+    
+    public abstract void computeTrustScore(float trustMax);
+    
+    public float getTrustScore()
+    {
+        return this.trustScore;
+    }
+    
+    public abstract String toString();
+    
+    public abstract BasicDBObject toDBObject();
+    
+}
